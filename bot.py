f2e0b1fc08dd00e9ebf15565e1f7eda96088a42f
Kurochkin Egor <willson2019.octa@mail.ru>
2024-12-04 19:40:46 +0400

    upd: aiogram==3.15.0

--- bot.py
+++ bot.py
@@ -1,28 +1,21 @@
-import logging
-
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from data.config import BOT_TOKEN
-from handlers.user import router as user_router
-from handlers.admin import router as admin_router
+from handlers.user_router import router as user_router
+from handlers.admin_router import router as admin_router
 from loader import loop
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] (%(funcName)s) %(message)s",
-    handlers=[
-        logging.FileHandler("logs/logs.log", encoding="utf-8"),
-        logging.StreamHandler()
-    ]
-    )
-
-
 async def main() -> None:
-    bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(
+        token=BOT_TOKEN, 
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
     dp = Dispatcher(storage=MemoryStorage())
+    
     dp.include_routers(user_router, admin_router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, skip_updates=False) 
